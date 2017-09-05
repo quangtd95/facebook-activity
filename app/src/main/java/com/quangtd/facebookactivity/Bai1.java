775c178726290210c966f4654895aab260e6b328
quang.td95 <quang.td95@gmail.com>
2017-09-05 16:13:20 +0700

    linnear layout 1

---
+++ app/src/main/java/com/quangtd/facebookactivity/Bai1.java
@@ -0,0 +1,18 @@
+package com.quangtd.facebookactivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Quang_TD on 9/5/2017.
+ */
+
+public class Bai1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_a);
+
+    }
+}
